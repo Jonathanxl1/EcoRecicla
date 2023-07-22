b3ad5e42126af1a3ad6f302f5e337450fb793767
j.aguasaco <j.aguasaco@mobiera.com>
2023-07-22 13:40:22 -0500

    Definiendo vistas, interacciones basicar en cada vistas y agregando recursos visuales.

---
+++ app/src/main/java/com/example/ecorecicla/RegisterProductsView.java
@@ -0,0 +1,14 @@
+package com.example.ecorecicla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterProductsView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_products_view);
+    }
+}
