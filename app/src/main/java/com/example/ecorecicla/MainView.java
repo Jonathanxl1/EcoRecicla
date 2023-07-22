cafa63d1a8457897e72ff0b44f8eee0f601c2775
j.aguasaco <j.aguasaco@mobiera.com>
2023-07-22 13:44:24 -0500

    Interacciones y botones relacionados funcionando, pendiente de logica operacional

--- app/src/main/java/com/example/ecorecicla/MainView.java
+++ app/src/main/java/com/example/ecorecicla/MainView.java
@@ -1,14 +1,60 @@
 package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class principalView extends AppCompatActivity {
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainView extends AppCompatActivity {
+    private FloatingActionButton addProduct;
+    private BottomAppBar bottomAppBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_view);
+        addProduct = findViewById(R.id.addProduct);
+        bottomAppBar = findViewById(R.id.bottomAppBar);
+
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pageNews = new Intent(getApplicationContext(),NewsViews.class);
+                startActivity(pageNews);
+            }
+        });
+
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int option = item.getItemId();
+                if (option == R.id.profileButton) {
+                    Intent profile = new Intent(getApplicationContext(), ProfileView.class);
+                    startActivity(profile);
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        });
+
+
+
+
+        addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registrarProducto = new Intent(getApplicationContext(), RegisterProductsView.class);
+                startActivity(registrarProducto);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ecorecicla/NewsViews.java
+++ app/src/main/java/com/example/ecorecicla/NewsViews.java
@@ -1,14 +1,42 @@
 package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class NewsViews extends AppCompatActivity {
+
+    private BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_views);
+        bottomAppBar = findViewById(R.id.bottomAppBar);
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //keep this position
+            }
+        });
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int option = item.getItemId();
+                if (option == R.id.profileButton) {
+                    Intent profile = new Intent(getApplicationContext(), ProfileView.class);
+                    startActivity(profile);
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        });
     }
 }
