5ca49b68916d19607853701017aab2bd5e260e0a
j.aguasaco <j.aguasaco@mobiera.com>
2023-08-19 21:29:49 -0500

    Added action to open map

--- app/src/main/java/com/example/ecorecicla/Views/MapView.java
+++ app/src/main/java/com/example/ecorecicla/Views/MapView.java
@@ -3,14 +3,25 @@ package com.example.ecorecicla.Views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.example.ecorecicla.R;
 
-public class mapView extends AppCompatActivity {
+public class MapView extends AppCompatActivity {
+
+    private WebView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_view);
+        init();
     }
+     private void init(){
+        mapView = findViewById(R.id.mapView);
+        WebSettings webSettings = mapView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mapView.loadUrl("https://mapareciclajeapp.netlify.app");
+     }
 }
