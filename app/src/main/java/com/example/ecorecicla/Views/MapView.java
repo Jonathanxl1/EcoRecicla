ec69aaa4fdacdb899429e1ac6dfb9bf4e4ebd99a
j.aguasaco <j.aguasaco@mobiera.com>
2023-08-19 21:24:54 -0500

    Added MapView show puntos de reciclaje and resource to set on topAppBar

---
+++ app/src/main/java/com/example/ecorecicla/Views/MapView.java
@@ -0,0 +1,16 @@
+package com.example.ecorecicla.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecorecicla.R;
+
+public class mapView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view);
+    }
+}
